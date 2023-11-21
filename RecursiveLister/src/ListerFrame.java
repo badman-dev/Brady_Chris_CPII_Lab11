05f02e2d47c77ead15c7cbc377b32842287af15f
badman-dev <budmandev@gmail.com>
2023-11-20 19:31:49 -0500

    finished app and updated doc

--- RecursiveLister/src/ListerFrame.java
+++ RecursiveLister/src/ListerFrame.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 
 public class ListerFrame extends JFrame {
     JPanel northPnl;
+    JLabel titleLbl;
 
     JPanel centerPnl;
     JButton openBtn;
@@ -25,7 +26,7 @@ public class ListerFrame extends JFrame {
         Dimension screenSize = tk.getScreenSize();
 
         setTitle("Random Product Search");
-        setSize(500, 400);
+        setSize(500, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -33,13 +34,17 @@ public class ListerFrame extends JFrame {
 
     private void createNorthPanel() {
         northPnl = new JPanel();
+        titleLbl = new JLabel("Directory Lister");
+        titleLbl.setFont(new Font("Serif", Font.PLAIN, 32
+        ));
 
+        northPnl.add(titleLbl);
         add(northPnl, BorderLayout.NORTH);
     }
     private void createCenterPanel() {
         centerPnl = new JPanel();
         openBtn = new JButton("Select Folder");
-        outputArea = new JTextArea(20, 20);
+        outputArea = new JTextArea(40, 40);
         outputScroll = new JScrollPane(outputArea);
 
         openBtn.addActionListener((ActionEvent ae) -> fileHandler());
@@ -68,14 +73,27 @@ public class ListerFrame extends JFrame {
             System.out.println("You chose to open this file: " +
                     chooser.getSelectedFile().getName());
             try {
-                readDirectory(chooser.getSelectedFile());
+                File file = chooser.getSelectedFile();
+                outputArea.append(file.getName() + "\n");
+                readDirectory(file, 1);
             } catch (Exception e) {
                 System.out.println(e);
             }
         }
     }
 
-    private void readDirectory(File dir) {
+    private void readDirectory(File dir, int level) {
         File[] files = dir.listFiles();
+        for (File file : files) {
+            String newLine = "";
+            for (int i = 0; i < level; i++) {
+                newLine += "     ";
+            }
+            newLine += file.getName() + "\n";
+            outputArea.append(newLine);
+            if (file.isDirectory()) {
+                readDirectory(file, level++);
+            }
+        }
     }
 }
